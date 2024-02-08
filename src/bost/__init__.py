ae9904119d7fff2e58180877450027073136879c
Leon Haffmans <49658102+lord-haffi@users.noreply.github.com>
2024-02-08 11:11:15 +0100

    📄Proper documentation (#45)
    * 📄Proper documentation
    
    * 🩹
    
    * Apply suggestions from code review

--- src/bost/__init__.py
+++ src/bost/__init__.py
@@ -1,3 +1,8 @@
 """
-src contains all your business logic
+BO4E-Schemas-Tool (`bost` as CLI) is a tool to conveniently download and modify the BO4E-Schemas.
+For more information, see the README.md file.
 """
+
+__all__ = ["main"]
+
+from .__main__ import main
